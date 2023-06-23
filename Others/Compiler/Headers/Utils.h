8c9f7d152c328b619ca0a4ce7ef83812d5d13f62
Mihai Razvan <MIHAIRAZVAN03@YAHOO.COM>
2023-06-23 15:55:50 +0300

    made compiler project

---
+++ Others/Compiler/Headers/Utils.h
@@ -0,0 +1,11 @@
+#include <vector>
+#include <string>
+
+#ifndef COMPILER_UTILS_H
+#define COMPILER_UTILS_H
+
+namespace utils {
+
+}
+
+#endif //COMPILER_UTILS_H
---
+++ Others/Compiler/Implementations/Utils.cpp
@@ -0,0 +1,11 @@
+#include "../Headers/utils.h"
+#include <string>
+#include <vector>
+#include <fstream>
+
+namespace utils {
+
+}
+
+
+
